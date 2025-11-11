f1ce85395b549b0d004f825a84e1df69ccd3fcbc
Tejas-Santosh-Nalawade <tejassantoshnalawade@gmail.com>
2025-11-12 01:08:44 +0530

    NOTES

--- PRACTICAL/CODE/DLL/ArithmeticJNI.java
+++ PRACTICAL/CODE/DLL/ArithmeticJNI.java
@@ -26,3 +26,34 @@ public class ArithmeticJNI {
         sc.close();
     }
 }
+
+
+// import java.util.*;
+
+// public class ArithmeticJNI{
+//     public native int add(int a, int b);
+//     public native int subtract(int a, int b);
+//     public native int multiply(int a, int b);
+//     public native double divide(int a, int b);
+
+//     static {
+//         System.loadLibrary("ArithmeticLib");
+//     }
+
+//     public static void main (String[] args) {
+
+//         ArithmeticJNI obj = new ArithmeticJNI();
+
+//         Scanner sc = new Scanner(System.in);
+
+//         System.out.println("Enter Any two Number:");
+//         int x= sc.nexInt();
+//         int y= sc.nexInt();
+
+//         System.out.println("Addition: " + obj.add(x,y));
+//         System.out.println("Substractio: " + obj.sub(x,y));
+//         System.out.println("Multiplication: " + obj.multiply(x,y));
+//         System.out.println("Division: " + obj.divide(x,y));
+//         sc.close();
+//     }
+// }
